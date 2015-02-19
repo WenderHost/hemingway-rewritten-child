a5b0f3efcf1607a02a5c4dff4e56f08e872fdf8e
Michael Wender <michael@michaelwender.com>
2015-02-19 10:05:33 -0500

    Setting max-width to 1024px for nosidebar-page.php template

--- functions.php
+++ functions.php
@@ -1,3 +1,26 @@
 <?php
 add_editor_style( 'lib/css/editor-style.css' );
+
+/**
+ * Adds editor styles based on the current page template.
+ *
+ * Hooked to `admin_enqueue_scripts`.
+ *
+ * @uses add_editor_style()
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function hemingway_child_add_editor_styles() {
+    global $post;
+
+    $template = basename( get_page_template() );
+
+    switch ( $template ) {
+    	case 'nosidebar-page.php':
+    		add_editor_style( 'lib/css/editor-style_nosidebar.css' );
+    		break;
+    }
+}
+add_action( 'admin_enqueue_scripts', 'hemingway_child_add_editor_styles' );
 ?>
